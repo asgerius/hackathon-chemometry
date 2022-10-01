c4299cd627d95d80d565aee5a585a6cf4ebd0fa3
chalpher <tjalfe@rocketmail.com>
2022-10-02 00:22:12 +0200

    logs and ders

--- src/preprocess.py
+++ src/preprocess.py
@@ -25,6 +25,37 @@ def combined_linear(data: Data):
     )
     return lr
 
+def get_logarithm(data: Data):
+    flatfeatures = data.features.reshape(-1,700)
+    flatfeatures = np.log(flatfeatures)
+    return flatfeatures.reshape(data.features.shape)
+
+def get_derivatives(data: Data):
+    flatfeatures = data.features.reshape(-1,700)
+    gradx = np.gradient(flatfeatures,axis=0)
+    return gradx.reshape(data.features.shape)
+
+def apply_derivatives(data: Data, derivatives: Data):
+    return np.concatenate((data,derivatives), axis=-1)
+
+def apply_logs(data: Data, logs: Data):
+    return np.concatenate((data,logs), axis=-1)
+
 def apply_combined_linear(data: Data, lr):
     """ INPLACE """
     data.features = data.features - (lr.slope * data.nm + lr.intercept)
+
+if __name__ == "__main__":
+
+    data = load_from_pickle()
+    lr = combined_linear(data)
+    apply_combined_linear(data, lr)
+    logs=get_logarithm(data)
+    ders=get_derivatives(data)
+    print(np.shape(data.features))
+    print(np.shape(ders))
+    data.features=apply_derivatives(data.features,ders)
+    print(np.shape(data.features))
+    print(logs.shape)
+    data.features = apply_logs(data.features,logs)
+    print(np.shape(data.features))
